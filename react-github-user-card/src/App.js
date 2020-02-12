ad1a5523988a97d043fd877bd0d72004a0c07183
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-12 02:44:21 -0500

    able to get follower users accs info rendering properly. needs styling

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
 
     this.state = {
       user: {},
-      followers: []
+      followers: [],
     };
 
   }
@@ -19,80 +19,90 @@ class App extends React.Component {
   componentDidMount() {
     console.log("component mounted");
 
-    //update state
+    //update user state
     axios.get('https://api.github.com/users/brandi-jones')
     .then(response => {
       console.log("componentDidMount response 1, brandi-jones:", response) 
 
       this.setState({
         user: response.data,
-        followers: getFollowers(response.data)
+        //followers: getFollowers(response.data)
       })
     })
     .catch(error => {
       console.log(error)
     })
 
-
-
-    //function to get followers of github user passed in
-    function getFollowers(user) {
-
-      let arrayFollowers = [];
-      const followersUrl = user.followers_url; //get users followers list
-      axios.get(followersUrl)
-      .then(response => {
-        console.log("getFollowers function axios response:", response);
-
-        //arrayFollowers = response.data; <-- commented out, this is the way if we wanted to only get an array of followers from user.followers_url. We need to get futher acc info for things like location, bio, etc of followers
-
-        response.data.forEach(follower => {
-          axios.get(follower.url)
-          .then(response => {
-            arrayFollowers.push(response.data)
-
-          })
-          .catch(error => {
-            console.log(error)
+    //update followers state
+    axios.get("https://api.github.com/users/brandi-jones/followers")
+    .then(response => {
+     
+      response.data.map(user => {
+      
+        axios.get(user.url)
+        .then(response => {
+         
+          this.setState({
+            followers: [...this.state.followers, response.data]
           })
         })
-
-        console.log("arrayFollowers = ", arrayFollowers);
-        console.log("array length", arrayFollowers.length)
+        .catch(error => {
+          console.log("ERROR:", error)
+        })
       })
-      .catch(error => {
-        console.log(error)
-      })
+      
 
-      return arrayFollowers;
-    }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+
+    // // //function to get followers of github user passed in
+    // function getFollowers(user) {
+
+    //   let arrayFollowers = [];
+    //   const followersUrl = user.followers_url; //get users followers list
+    //   axios.get(followersUrl)
+    //   .then(response => {
+    //     console.log("getFollowers function axios response:", response.data);
+
+    //     //arrayFollowers = response.data; // commented out, this is the way if we wanted to only get an array of followers from user.followers_url. We need to get futher acc info for things like location, bio, etc of followers
+
+    //     response.data.forEach(follower => {
+    //       axios.get(follower.url)
+    //       .then(response => {
+    //         arrayFollowers.push(response.data)
+
+    //       })
+    //       .catch(error => {
+    //         console.log(error)
+    //       })
+    //     })
+
+    //     console.log("arrayFollowers = ", arrayFollowers);
+    //     console.log("array length", arrayFollowers.length)
+    //   })
+    //   .catch(error => {
+    //     console.log(error)
+    //   })
+
+    //   return arrayFollowers;
+    // }
     
 
   }
 
+ 
 
 
   render() {
     console.log('rendering app...', this.state.followers)
     return (
       <>
-        <p>test</p>
-        {/*<CardList followers={this.state.followers} />*/}
-
         <Card follower={this.state.user}/>
-
-        <div className="cardList">
-            <h2>Followers:</h2>
-            {console.log("state right before map in app: ", this.state.followers)}
-            {console.log("length of array in state: ", this.state.followers.length)}
-            {this.state.followers.map(user => {
-               return(
-                <Card key={user.id} follower={user}/>
-                
-               ) 
-            })}
-
-        </div>
+        <CardList followers={this.state.followers} />
+    
       </>
     )
   }
--- react-github-user-card/src/components/CardList.js
+++ react-github-user-card/src/components/CardList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card.js";
+import axios from "axios";
 
 function CardList(props) {
     console.log("cardList props: ", props)
@@ -20,3 +21,40 @@ function CardList(props) {
 }
 
 export default CardList;
+
+
+// class CardList extends React.Component {
+    
+//     constructor () {
+//         super();
+//         this.state = {
+//             followerAccs: []
+//         };
+//     }
+
+//     componentDidMount() {
+//         console.log("COMPONENT MOUNTED IN CARDLIST")
+//         console.log(this.props.followers.length)
+        
+
+//     }
+
+//     render() {
+//         console.log("cardList props: ", this.props.followers)
+//         return (
+//             <div className="cardList">
+//                 <h2>Followers:</h2>
+
+//                 {this.props.followers.map(user => {
+//                 return(
+//                     <Card key={user.id} follower={user}/>
+                    
+//                 ) 
+//                 })}
+
+//             </div>
+//         );
+//     }
+// }
+
+// export default CardList;
